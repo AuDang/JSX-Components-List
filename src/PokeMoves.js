3c03ec69a9066d83f854c55fa9639381936b9771
Austin Dang <austindang415@gmail.com>
2022-03-22 12:35:50 -0700

    first commit

--- src/PokeMoves.js
+++ src/PokeMoves.js
@@ -6,7 +6,8 @@ const PokeMoves = ()=> {
     <div>
         <h1>PokeMoves</h1>
         <ul>
-            {moves.map(item => (
+            {isLogged &&
+            moves.map(item => (
                 // <li>
                 //     {item.id}. {item.move}
                 // </li>
