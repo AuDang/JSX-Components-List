4787322002fef87f7b489581f84b9b9726272041
Austin Dang <austindang415@gmail.com>
2022-03-22 11:06:49 -0700

    logged

--- src/PokeMoves.js
+++ src/PokeMoves.js
@@ -1,17 +1,19 @@
 import {moves} from './data'
 
 const PokeMoves = ()=> {
-
+    const isLogged = true
     return (
     <div>
         <h1>PokeMoves</h1>
         <ul>
-            {moves.map(item => (
+            {is Logged 
+            ? moves.map(item => (
                 // <li>
                 //     {item.id}. {item.move}
                 // </li>
                 <PokeMoveCard key={item.id} item ={item} />
-            ))}
+            ))
+            : "Not Logged In"}
         </ul>
     </div>
 )
